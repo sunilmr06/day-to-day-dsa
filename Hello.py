64818011bb39cdeb77b14633ea1b0a259cc46a1d
sunilmr06 <smr662003@gmail.com>
2025-11-06 18:43:35 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+#Date : 06/11/2025 
+# day 1: Print Hello with  user input.
+
+#input function is helps to get data form the user and returns the data in string formate  
+name = input("Place Enter Your name:")
+
+#print statement help to print the output
+
+print("Hello, have a nice day ",name)
