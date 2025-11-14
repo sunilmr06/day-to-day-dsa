a11415642a6e400ed3f197f852cc32aa8697cbc7
sunilmr06 <smr662003@gmail.com>
2025-11-14 22:04:13 +0530

    Add files via upload

---
+++ reverse_number.py
@@ -0,0 +1,24 @@
+# Date : 14/11/2025
+# day - 9 Reverse A Number 
+
+def reverse_number(n):
+    original = n
+    reverse = 0
+
+    while n > 0 :
+        digit = n % 10
+        reverse = reverse * 10 + digit
+        n //= 10
+
+    print(f"Reverse of {original} is {reverse}")
+
+num = int(input("Enter a number : "))
+print(reverse_number(num))
+
+def reverse_string(n) :
+    reverse = n[::-1]
+
+    print(f"Reverse of {n} is {reverse}")
+
+value = input("Enter a string : ")
+print(reverse_string(value))
