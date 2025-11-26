119e3338219577dde711aa2336f3577852ded552
sunilmr06 <smr662003@gmail.com>
2025-11-26 22:39:33 +0530

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,19 @@
+# Date : 26/11/2025
+# Day 16 Insertion sort 
+
+def insertion_sort(arr) :
+    n = len(arr)
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key :
+            arr[j + 1] = arr[j]
+            j-= 1
+
+        arr[j + 1] = key
+    return arr
+
+array = list(map(int, input("Enter a array element :").split()))
+print(insertion_sort(array))
